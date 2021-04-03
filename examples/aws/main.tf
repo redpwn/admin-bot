be63b261e75914dc86d020f30c91d00648efe837
Philip Papurt <ginkoid@gmail.com>
2021-04-03 06:02:08 -0400

    fix: use ecr for example image

--- examples/aws/main.tf
+++ examples/aws/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 
 module "admin_bot" {
   source = "redpwn/admin-bot/aws"
-  image  = "redpwn/admin-bot-example"
+  image  = "345804057348.dkr.ecr.us-east-1.amazonaws.com/redpwn/admin-bot/example@sha256:5dda2e698c8e1930e5e4d1c9922244b697d0a6e47d5138deced1fa1865c700dc"
   recaptcha = {
     site   = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
     secret = "6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe"
